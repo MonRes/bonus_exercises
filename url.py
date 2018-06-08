23aca59b9dde9256411f86c5be4abd2cde93c10b
Restek <Restek87@gmail.com>
2018-06-08 23:05:42 +0200

    Initial commit

--- url.py
+++ url.py
@@ -7,26 +7,28 @@ Zdefiniować przykładową listę z kilkoma opisanymi powyżej rekordami. Napisa
 który dla każdego urla wypisze ilość udanych oraz nieudanych zapytań, a także średnią czasu odpowiedzi
 zapytań, które się nie powiodły"""
 
-http_record = [{'url' : 'http//google.pl', 'status': 'TRUE', 'response_time': 30},
-               {'url' : 'http//wp.pl', 'status': 'FALSE', 'response_time': 22},
-                {'url': 'http//google.pl', 'status': 'FALSE', 'response_time': 32},
-               {'url': 'http//google.pl', 'status': 'FALSE', 'response_time': 12},
-               {'url': 'http//wp.pl', 'status': 'TRUE', 'response_time': 50},
-               {'url': 'http//wp.pl', 'status': 'TRUE', 'response_time': 53}]
+http_record = [{'url' : 'http//google.pl', 'status': True, 'response_time': 30},
+               {'url' : 'http//wp.pl', 'status': False, 'response_time': 22},
+                {'url': 'http//google.pl', 'status': False, 'response_time': 32},
+               {'url': 'http//google.pl', 'status': False, 'response_time': 12},
+               {'url': 'http//wp.pl', 'status': True, 'response_time': 50},
+               {'url': 'http//wp.pl', 'status': True, 'response_time': 53}]
 
 
 total_by_url = {}
-count_by_url = {}
 
 for record in http_record:
     url = record['url']
-    if url in total_by_url:
-        total_by_url[url] += record['status']
-        count_by_url[url] += 1
-    else:
-        total_by_url[url] = record['status']
-        count_by_url[url] = 1
-for url in count_by_url:
-    print(count_by_url)
+    if url not in total_by_url:
+        total_by_url[url] = {'True': 0, 'False': 0, 'ResponseTime': 0}
 
+    if record['status'] == True:
+        total_by_url[url]['True'] += 1
+        total_by_url[url]['ResponseTime'] += record['response_time']
+    else:
+        total_by_url[url]['False'] += 1
+for x in total_by_url:
+    total_by_url[x]['ResponseTime'] = total_by_url[x]['ResponseTime'] / total_by_url[x]['True']
+
+print(total_by_url)
 
